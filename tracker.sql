84c96f4e9ceb22d8cbe5596c8be8965633a81d8a
Thokozile Soko <thokozile.soko@yahoo.com>
2021-02-06 18:04:25 -0500

    Hard coded some data in mysql to see how the data shows up.

--- tracker.sql
+++ tracker.sql
@@ -21,8 +21,23 @@ id INT PRIMARY KEY,
 first_name varChar (30) not null,
 last_name varChar (30) not null,
 role_id INT not null,
-manager_id INT not null
-
-
-
+manager_id INT null
 );
+
+INSERT INTO department (name) VALUES ("Legal");
+INSERT INTO department (name) VALUES ("Human Resources");
+INSERT INTO department (name) VALUES ("Accounting");
+
+SELECT * FROM department;
+
+INSERT INTO roles (title, salary, department_id) VALUES ("Partner", 95000, 1);
+INSERT INTO roles (title, salary, department_id) VALUES ("Head of HR", 90000, 2);
+INSERT INTO roles (title, salary, department_id) VALUES ("Manager",95000, 3);
+
+SELECT * FROM role;
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("John", "Doe", 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Peter", "Rabbit", 2, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Jane", "Austen", 3, 3);
+
+SELECT * FROM employee;
