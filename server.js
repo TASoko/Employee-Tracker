2371079ac4e6ae2673b3f5636e5d6883295be119
Thokozile Soko <thokozile.soko@yahoo.com>
2021-02-07 18:03:11 -0500

    I deleted all commented out code because I won't need them. I was able to prompt a user to choose what they want to do, so far only add works. At add they can choose department, then they can input a new department. The department this input consoles. Need to have it render in sql.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //Dependencies
-require('dotenv').config() //This is for the .env file that will hide my password
+require('dotenv').config(); //This is for the .env file that will hide my password
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 const cTable = require('console.table');
@@ -20,7 +20,7 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  runSearch();
+  start();
 });
 
 function start () {
@@ -71,7 +71,7 @@ function add () {
         ]
       })
       .then(function(answer) {
-        switch (answer.action) {
+        switch (answer.add) {
         case "Department":
           addDepartment();
           break;
@@ -99,131 +99,12 @@ function add () {
         message: "Add a department?",
        })
       .then(function(answer) {
-     connection.query("SELECT * FROM department WHERE ?", { song: answer.song }, function(err, res) {
-
-       
-      });
-  }
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function addEmployee() {
-//   inquirer
-//     .prompt({
-//       name: "employee",
-//       type: "input",
-//       message: "?"
-//     })
-//     .then(function(answer) {
-//       var query = "SELECT position, song, year FROM top5000 WHERE ?";
-//       connection.query(query, { artist: answer.artist }, function(err, res) {
-//         if (err) throw err;
-//         for (var i = 0; i < res.length; i++) {
-//           console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
-//         }
-//         start();
-//       });
-//     });
-// }
-  
-
-// function viewEmployees() {
-//   var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
-//   connection.query(query, function(err, res) {
-//     if (err) throw err;
-//     for (var i = 0; i < res.length; i++) {
-//       console.log(res[i].artist);
-//     }
-//     start();
-//   });
-// }
-
-// function updateEmployeemaybe() {
-//   inquirer
-//     .prompt([
-//       {
-//         name: "start",
-//         type: "input",
-//         message: "Enter starting position: ",
-//         validate: function(value) {
-//           if (isNaN(value) === false) {
-//             return true;
-//           }
-//           return false;
-//         }
-//       },
-//       {
-//         name: "end",
-//         type: "input",
-//         message: "Enter ending position: ",
-//         validate: function(value) {
-//           if (isNaN(value) === false) {
-//             return true;
-//           }
-//           return false;
-//         }
-//       }
-//     ])
-//     .then(function(answer) {
-//       var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-//       connection.query(query, [answer.start, answer.end], function(err, res) {
-//         if (err) throw err;
-//         for (var i = 0; i < res.length; i++) {
-//           console.log(
-//             "Position: " +
-//               res[i].position +
-//               " || Song: " +
-//               res[i].song +
-//               " || Artist: " +
-//               res[i].artist +
-//               " || Year: " +
-//               res[i].year
-//           );
-//         }
-//         start();
-//       });
-//     });
-// }
-
-// function updateEmployee() {
-//   inquirer
-//     .prompt({
-//       name: "update-employee",
-//       type: "input",
-//       message: "Who would you like to update?"
-//     })
-//     .then(function(answer) {
-//       console.log(answer);
-//       connection.query("SELECT * FROM employee WHERE ?", { song: answer.song }, function(err, res) {
-//         if (err) throw err;
-//         console.log(
-//           "Position: " +
-//             res[0].position +
-//             " || Song: " +
-//             res[0].song +
-//             " || Artist: " +
-//             res[0].artist +
-//             " || Year: " +
-//             res[0].year
-//         );
-//         start();
-//       });
-//     });
-// }
+     connection.query("INSERT INTO department (name) VALUES (?)",
+      { name: answer.department},
+       function(err) {
+        if (err) throw err;
+         console.log(answer.department);
+      start ();
+       }
+      )});
+    }
